79c0db139f3d1ca00b0fcce7a1acc5a493929403
ZombieGenZ <138541500+ZombieGenZ@users.noreply.github.com>
2025-11-23 18:47:21 +0700

    Serialize enums as strings for cart requests

--- Server/Assignment/Program.cs
+++ Server/Assignment/Program.cs
@@ -7,11 +7,15 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
